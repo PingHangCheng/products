09e995b206ab0ec17e11ce68feab0889c0b948b2
PingHuangCheng <cheng553490@gmail.com>
2021-08-17 21:34:22 +0800

    add products.py

--- products.py
+++ products.py
@@ -19,15 +19,15 @@
 #     products.append(p)
 # print(products)
 #二維清單_版本2
-products = []
-while True:
-    name = input('請輸入商品名稱：')
-    if name == 'q':
-        break
-    price = input('請輸入商品價格：')
-    p = [name, price]
-    products.append(p)
-print(products)
+# products = []
+# while True:
+#     name = input('請輸入商品名稱：')
+#     if name == 'q':
+#         break
+#     price = input('請輸入商品價格：')
+#     p = [name, price]
+#     products.append(p)
+# print(products)
 #二維清單_版本3
 products = []
 while True:
