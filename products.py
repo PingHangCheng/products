a5c16d0fbd8b4aa2904e472cb8c7fd6022397546
PingHuangCheng <cheng553490@gmail.com>
2021-09-10 18:43:30 +0800

    add header

--- products.py
+++ products.py
@@ -35,9 +35,21 @@ while True:
     if name == 'q':
         break
     price = input('請輸入商品價格：')
+    price = int(price)
     products.append([name, price])
 print(products)
 
 for p in products:
     # print(p[0]) 會印出小清單的商品名稱
     print(p[0], "的價格是", p[1] )
+
+# with open ('product.csv', 'w') as f:
+#     f.write('商品, 價格 \n')
+#     for p in products:
+#         f.write(p[0] + ',' + str(p[1]) + '\n')
+
+#如果出現亂碼時，沒有亂碼，就不用，寫入檔案時加入encoding='utf-8' 來解決中文字變亂碼問題, = 兩邊不要空格
+with open ('product.csv', 'w', encoding='utf-8') as f:
+    f.write('商品, 價格 \n')
+    for p in products:
+        f.write(p[0] + ',' + str(p[1]) + '\n')
