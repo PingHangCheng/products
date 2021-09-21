47c2f84b902e8a292bb44a66a2f8c402bf5b45f3
PingHuangCheng <cheng553490@gmail.com>
2021-09-21 20:39:08 +0800

    add read file

--- products.py
+++ products.py
@@ -1,3 +1,14 @@
+#第65課讀取檔案
+products = []
+with open('product.csv', 'r', encoding='utf-8') as f:
+    for line in f:
+        # s= line.strip().split(',')
+        # print(s)
+        if '商品, 價格' in line:
+            continue
+        name, price = line.strip().split(',')
+        products.append([name, price])
+print(products)
 #一維清單
 # products = []
 # while True:
@@ -29,7 +40,7 @@
 #     products.append(p)
 # print(products)
 #二維清單_版本3
-products = []
+#讓使用者輸入
 while True:
     name = input('請輸入商品名稱：')
     if name == 'q':
@@ -39,15 +50,16 @@ while True:
     products.append([name, price])
 print(products)
 
+#印出所有購買紀錄
 for p in products:
     # print(p[0]) 會印出小清單的商品名稱
     print(p[0], "的價格是", p[1] )
 
+#寫入檔案
 # with open ('product.csv', 'w') as f:
 #     f.write('商品, 價格 \n')
 #     for p in products:
 #         f.write(p[0] + ',' + str(p[1]) + '\n')
-
 #如果出現亂碼時，沒有亂碼，就不用，寫入檔案時加入encoding='utf-8' 來解決中文字變亂碼問題, = 兩邊不要空格
 with open ('product.csv', 'w', encoding='utf-8') as f:
     f.write('商品, 價格 \n')
